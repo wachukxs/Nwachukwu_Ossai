08579817efa3ce4f7ffa0621bc1e1f4e93e1ebb4
Ossai Nwachukwu <nwachukwuossai@gmail.com>
2024-09-10 01:06:06 +0100

    initial commit - problem 3 first draft.

--- readme.md
+++ readme.md
@@ -1,3 +1,16 @@
+Backend submission - https://99tech.notion.site/8dafab98dfaa4365ae7b60ccf8f9b40e?v=76218a75eb3b411fb048a3552d3ed236&pvs=4
+
+See problems, 3,4, and 5.
+
+To execute, for instance run;
+```cmd
+npx tsx index.ts
+```
+
+- - -
+> Cloned from https://github.com/99techteam/code-challenge/tree/main
+
+
 # 99Tech Code Challenge #1 #
 
 Note that if you fork this repository, your responses may be publicly linked to this repo.  
---
+++ src/problem3/index.ts
@@ -0,0 +1,59 @@
+
+/**
+ * TODO: Comment on the complexity or efficiency of each function.
+ * 
+ * I'm assuming we're only concerned with positive numbers.
+ * Given a negative number, it won't make sense to sum negative numbers and move in the "opposite direction"
+ * I also won't bother with zero.
+ */
+
+function sum_to_n_a(n: number): number {
+    let total: number = 0
+    if (Number.isNaN(n) || n < 1) {
+        return total
+    }
+	// use while loop
+
+    total = total + n
+    while ((n = n - 1) > 0) {
+        total = total + n
+    }
+
+    return total
+}
+
+function sum_to_n_b(n: number): number {
+    let total: number = 0
+    if (Number.isNaN(n) || n < 1) {
+        return total
+    }
+	// use for loop
+
+    for (n; n > 0; n--) {
+        total = total + n
+    }
+    return total
+}
+
+// function sum_to_n_c(n: number, total: number = 0): number {
+//     if (Number.isNaN(n) || n < 1) {
+//         return total
+//     }
+//     total = total + n
+//     n = n - 1
+//     return sum_to_n_c(n, total)
+// 	// use recursion
+// }
+
+function sum_to_n_c(n: number): number {
+    if (Number.isNaN(n) || n < 1) {
+        return 0
+    }
+    let total = n + sum_to_n_c(n - 1)
+    return total
+	// use recursion
+}
+
+// console.log(sum_to_n_a(5))
+// console.log(sum_to_n_b(5))
+// console.log(sum_to_n_c(5))
